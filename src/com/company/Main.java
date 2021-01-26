6236dc08ea82f45d3b72e2b2bbd062ff4106e519
mcndoungerii <allenmcndounger@gmail.com>
2021-01-26 20:02:13 +0300

    Relational Operators

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,9 +9,29 @@ public class Main {
         //Multiplication == *
         //Division == /
         //Remainder == %
+
+    // Relational ops
+        // == - Equals to or Comparison Operator.
+        // != - Not Equal
+        // > - Greater than
+        // < - Less than
+        // >= - Greater than or Equal to
+        // <= - Less than or Equal to
+
         double firstNum = 36;
         double secondNum = 5;
 
-        System.out.println("The Remainder is "+ (firstNum % secondNum));
+        double result = 0;
+        result = firstNum % secondNum;
+
+        //If Statements
+        if(firstNum <= secondNum) {
+            System.out.println("The Remainder is "+ result);
+        }
+        else {
+            System.out.println("Numbers aren't equal");
+        }
+
+
     }
 }
