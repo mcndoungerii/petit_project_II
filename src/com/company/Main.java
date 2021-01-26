afbeb5fb2bfda19c5d81b70396ab58d8b82d6b84
mcndoungerii <allenmcndounger@gmail.com>
2021-01-26 19:30:28 +0300

    Basic ops

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Basics ops
+        //Addition == +
+        //Subtraction == -
+        //Multiplication == *
+        //Division == /
+        //Remainder == %
+        double firstNum = 36;
+        double secondNum = 5;
+
+        System.out.println("The Remainder is "+ (firstNum % secondNum));
+    }
+}
