1cd29303ae0edbecde20c6cf3b12c73f9382e770
mcndoungerii <allenmcndounger@gmail.com>
2021-01-27 22:30:57 +0300

    For loop

---
+++ src/com/company/Petit.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Petit {
+
+    public static void main(String[] args){
+
+        /*for loop
+                while loop
+
+         */
+
+        //for loop
+        for(int i = 0; i < 20 ; i++) {
+            if(i%2 == 0) {
+                System.out.println(i + " is a multiple of 2");
+            }
+
+        }
+    }
+}
