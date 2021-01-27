5fd2fbf7a45476b10f873662de3ade6617a3e890
mcndoungerii <allenmcndounger@gmail.com>
2021-01-27 22:37:23 +0300

    For and While loop

--- src/com/company/Petit.java
+++ src/com/company/Petit.java
@@ -10,11 +10,18 @@ public class Petit {
          */
 
         //for loop
-        for(int i = 0; i < 20 ; i++) {
-            if(i%2 == 0) {
-                System.out.println(i + " is a multiple of 2");
-            }
+//        for(int i = 0; i < 20 ; i++) {
+//            if(i%2 == 0) {
+//                System.out.println(i + " is a multiple of 2");
+//            }
+//
+//        }
 
+        //While loop
+        int i = 0;
+        while(i<5) {
+            System.out.println("Counting ..." +i);
+            i++;
         }
     }
 }
